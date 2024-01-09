61d621ef8442b67f629b5bc8918397cd05f52e36
akd4850 <akd3850@gmail.com>
2024-01-09 09:41:57 +0900

    2019 카카오 개발자 겨울 인턴십 > 크레인 인형뽑기 게임

--- coding_test/src/ct_lv1_java_64061/MainClass.java
+++ coding_test/src/ct_lv1_java_64061/MainClass.java
@@ -5,58 +5,69 @@ public class MainClass
 {
 	public static void main(String[] args) 
 	{
+		/*int[][] board = {
+					{0,0,0,0,0},
+					{0,0,1,0,3},
+					{0,2,5,0,1},
+					{4,2,4,4,2},
+					{3,5,1,3,1}
+				};*/
 		int[][] board = {
-				{0,0,0,0,0},
-				{0,0,1,0,3},
-				{0,2,5,0,1},
-				{4,2,4,4,2},
-				{3,5,1,3,1}};
-		int[] moves = {1,5,3,5,1,2,1,4};
+				{1, 0, 0, 0, 0},
+				{2, 0, 0, 0, 0},
+				{2, 0, 0, 0, 0},
+				{1, 0, 0, 0, 0},
+				{3, 0, 0, 0, 0}
+			};
+		//int[] moves = {1,5,3,5,1,2,1,4};
+		int[] moves = {1, 1, 1, 1};
 		System.out.println(solution(board, moves));
 	}
 	
 	public static int solution(int[][] board, int[] moves) 
 	{
-		List< Stack<Integer> > boardList = new ArrayList<>();
-		Queue<Integer> queueList = new LinkedList<>();
+		List<Integer> pickList = new ArrayList<>();
 		
-		for(int i = 0; i < board.length; i++) boardList.add( new Stack<Integer>() );
-		
-		for(int i = board.length - 1; i >= 0; i--)
-			for(int j = 0; j < board[ i ].length; j++)
-				boardList.get( j ).push( board[ i ][ j ] );
-				
 		for(int i = 0; i < moves.length; i++)
 		{
-			while( boardList.get( moves[ i ] - 1 ).isEmpty() == false )
+			for(int j = 0; j < board.length; j++)
 			{
-				int r = boardList.get( moves[ i ] - 1 ).pop();
-				if( r > 0 )
+				if( board[ j ][ moves[ i ] - 1 ] > 0 )
 				{
-					queueList.add( r );
+					pickList.add( board[ j ][ moves[ i ] - 1 ] );
+					board[ j ][ moves[ i ] - 1 ] = 0;
 					break;
 				}
 			}
 		}
-		
-		// 중간에 터지고 나서 다시 만나는 경우가 있음 /////// ------------------ 내일 다시 계산해보자
-		// 큐 방식 말고 다른 방식으로 처리해야 함!!!!!!!!!!!!!
+
 		int bombCount = 0;
-		int temp = 0;
-		while( queueList.isEmpty() == false )
+		
+		return GetUntilNotDistinct(pickList, bombCount);
+    }
+	
+	public static int GetUntilNotDistinct(List<Integer> pickList, int bombCount)
+	{
+		int index = 0;
+		
+		if(pickList.size() == 0) return bombCount;
+		
+		for(Integer i : pickList)
 		{
-			if( temp > 0 )
+			if(index == pickList.size() - 1) return bombCount;
+			
+			if(i == pickList.get( index + 1) )
 			{
-				int r = queueList.poll();
-				if(r == temp)
-				{
-					bombCount += 2;
-					temp = 0;
-				}
+				pickList.remove( index + 1);
+				pickList.remove( index );
+				bombCount += 2;
+				
+				return GetUntilNotDistinct( pickList, bombCount );
 			}
-			else temp = queueList.poll();
+			
+			index++;
 		}
 		
-		return bombCount;
-    }
+		return 0;
+	}
 }
